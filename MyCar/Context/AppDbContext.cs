4ac32d61433510d2d3c963e01a9a797effd6df47
JoyceCosta <joycecosta90@gmail.com>
2022-10-03 21:58:47 +0100

    AppDbContext class

---
+++ MyCar/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyCar.Models;
+
+namespace MyCar.Context
+{
+    public class AppDbContext : DbContext
+    { 
+        public DbSet<Car> Cars { get; set; } 
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\\mssqllocaldb;Database=Mycar;Trusted_Connection=True;MultipleaActiveResultSets=true");
+        }
+    }
+}
