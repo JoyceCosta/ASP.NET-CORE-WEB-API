58d79f554e03cc09fd8a0bfe9fbe9c0d3f85717f
JoyceCosta <joycecosta90@gmail.com>
2022-10-14 22:41:18 +0100

    methods

--- MyCar/Controllers/CarsController.cs
+++ MyCar/Controllers/CarsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MyCar.Context;
+using MyCar.Models;
 using System.Threading.Tasks;
 
 namespace MyCar.Controllers
@@ -25,6 +26,19 @@ namespace MyCar.Controllers
             {
                 success = true, 
                 data = await _appDbContext.Cars.ToListAsync()
+            });
+        } 
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCar(Car car)
+        {   
+
+            _appDbContext.Cars.Add(car);
+            await _appDbContext.SaveChangesAsync();
+            return Ok(new
+            {
+                success = true,
+                data = car
             });
         }
     }
